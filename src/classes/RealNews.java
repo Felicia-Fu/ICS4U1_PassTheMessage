f950fd41ad271cd121ee0099924f1a49a291c2ac
342889201 <342889201@C-43M5JB2.tdsb.on.ca>
2025-12-10 11:51:15 -0500

    Finished RealNews class

--- src/classes/RealNews.java
+++ src/classes/RealNews.java
@@ -9,5 +9,15 @@ package classes;
  * @author FFC03
  */
 public class RealNews extends News{
-    
+    private String url;
+    public RealNews(String headline, String contents, String author, String date, String newsSource, String type, String category, String url){
+        super(headline, contents, author, date, newsSource, type, category);
+        this.url = url;
+    }
+    @Override
+    public String information(){
+        return """
+               This is a real news
+               Learn more at: """ + url;
+    }
 }
