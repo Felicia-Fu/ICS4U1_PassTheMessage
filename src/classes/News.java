c1640c12a1684e339e336e8b7e36dc6bfd0afaf7
342889201 <342889201@C-43M5JB2.tdsb.on.ca>
2025-12-12 10:56:48 -0500

    Added comments

--- src/classes/News.java
+++ src/classes/News.java
@@ -8,7 +8,9 @@ package classes;
  *
  * @author Jia Xin Li
  */
+
 public class News {
+    //Created variables
     private String headline;
     private String contents;
     private String author;
@@ -16,6 +18,17 @@ public class News {
     private String newsSource;
     private String type;
     private String category;
+    
+    /**
+     * Constructor for the News class
+     * @param type
+     * @param category
+     * @param headline
+     * @param contents
+     * @param author
+     * @param date
+     * @param newsSource
+     */
     public News(String type, String category, String headline, String contents, String author, String date, String newsSource){
         this.headline = headline;
         this.contents = contents;
@@ -25,12 +38,27 @@ public class News {
         this.type = type;
         this.category = category;
     }
+    
+    /**
+     * Method to return real news information
+     * @return
+     */
     public String information(){
         return "This is a news";
     }
+    
+    /**
+     * Getter method to get news type (real)
+     * @return
+     */
     public String getType(){
         return type;
     }
+
+    /**
+     * Override toString Method for constant output
+     * @return
+     */
     @Override
     public String toString(){
         return "Headline: " + headline + "\nContent: " + contents + "\nAuthor: " + author + "\nDate: " + date + "\nNews Source: " + newsSource + "\nCategory: " + category;
